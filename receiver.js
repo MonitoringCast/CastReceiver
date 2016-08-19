a2e0686cf0682c7b54de84c0be04249234546b11
kibbet <github@kibbet.com>
2016-08-19 18:46:19 +0000

    send correct json to ws, move script elements out of head

--- receiver.js
+++ receiver.js
@@ -1,7 +1,7 @@
 var ctx = document.getElementById("myChart");
-window.data = [];
+var data = [];
 
-window.scatterChart = new Chart(ctx, {
+var scatterChart = new Chart(ctx, {
 	type: 'line',
 	data: {
 		datasets: [{
@@ -25,7 +25,7 @@ window.scatterChart = new Chart(ctx, {
 			pointRadius: 1,
 			pointHitRadius: 10,
 			label: 'Test Set 2',
-			data: window.data
+			data: data
 		}]
 	},
 	options: {
@@ -45,26 +45,26 @@ window.scatterChart = new Chart(ctx, {
 
 window.onload = function() {
 	cast.receiver.logger.setLevelValue(0);
-	window.castReceiverManager = cast.receiver.CastReceiverManager.getInstance();
+	var castReceiverManager = cast.receiver.CastReceiverManager.getInstance();
 	console.log('Starting Receiver Manager');
 			
 	// handler for the 'ready' event
 	castReceiverManager.onReady = function(event) {
 		console.log('Received Ready event: ' + JSON.stringify(event.data));
-		window.castReceiverManager.setApplicationState("Application status is ready...");
+		castReceiverManager.setApplicationState("Application status is ready...");
 	};
 			
 	// handler for 'senderconnected' event
 	castReceiverManager.onSenderConnected = function(event) {
 		console.log('Received Sender Connected event: ' + event.data);
-		console.log(window.castReceiverManager.getSender(event.data).userAgent);
+		console.log(castReceiverManager.getSender(event.data).userAgent);
 		// TODO: add here tje connection to the backend
 	};
 			
 	// handler for 'senderdisconnected' event
 	castReceiverManager.onSenderDisconnected = function(event) {
 		console.log('Received Sender Disconnected event: ' + event.data);
-		if (window.castReceiverManager.getSenders().length == 0) {
+		if (castReceiverManager.getSenders().length == 0) {
 			window.close();
 		}
 	};
@@ -75,11 +75,11 @@ window.onload = function() {
 	};
 
 	// create a CastMessageBus to handle messages for a custom namespace
-	window.messageBus = window.castReceiverManager.getCastMessageBus('urn:x-cast:com.google.cast.sample.helloworld');
-	window.messageBus2 = window.castReceiverManager.getCastMessageBus('urn:x-cast:message2');
+	var messageBus = castReceiverManager.getCastMessageBus('urn:x-cast:com.google.cast.sample.helloworld');
+	var messageBus2 = castReceiverManager.getCastMessageBus('urn:x-cast:message2');
 
 	// handler for the CastMessageBus message event
-	window.messageBus.onMessage = function(event) {
+	messageBus.onMessage = function(event) {
 		console.log('Message [' + event.senderId + ']: ' + event.data);
 		// inform all senders on the CastMessageBus of the incoming message event
 		// sender message listener will be invoked
@@ -87,25 +87,26 @@ window.onload = function() {
 		//TODO: we need to receive JSON. We need to check window.ws to see if it is open.
 		console.log(event.data);
 
-		window.ws = new WebSocket(event.data, []);
-		window.ws.onopen = function() {
+//		var ws = new WebSocket(event.data, []);
+		var ws = new WebSocket("ws://moncalliste.com:8020/graph", []);
+		ws.onopen = function() {
 			console.log("WEBSOCKET OPEN");
 			// Web Socket is connected, send data using send()
-			window.ws.send('{"message": "THIS IS ME!"}');
+			ws.send('{"user": "user1","pass":"pass1","layout":"layout1"}');
 			alert("connection open");
 		};
 
-		window.ws.onmessage = function (evt) { 
+		ws.onmessage = function (evt) { 
 			var received_msg = JSON.parse(evt.data);
-			window.data.push(received_msg)
-			window.scatterChart.update();
-			console.log(window.data);
+			data.push(received_msg)
+			scatterChart.update();
+			console.log(data);
 		};
 
-		window.messageBus.send(event.senderId, event.data);
+		messageBus.send(event.senderId, event.data);
 	};
 
 	// initialize the CastReceiverManager with an application status message
-	window.castReceiverManager.start({statusText: "Application is starting"});
+	castReceiverManager.start({statusText: "Application is starting"});
 	console.log('Receiver Manager started');
 };
